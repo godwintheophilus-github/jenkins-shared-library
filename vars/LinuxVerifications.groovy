76cbf563333b54a9074347a9bcc1879b10e92e16
gdwntheophilus <85812329+gdwntheophilus@users.noreply.github.com>
2024-07-29 22:58:34 +0530

    debugging

--- vars/LinuxVerifications.groovy
+++ vars/LinuxVerifications.groovy
@@ -42,44 +42,44 @@ def call(body) {
                             echo "${env.WORKSPACE}"
                         }
                     }
-                    post {
-                        always {
-                            echo 'This will always run'
-                            script {
-                                helper.checkWhoAmI(env: params.env, branch: params.branch)
-                            }
-                        }
-                        success {
-                            echo 'This will run only if successful'
-                        }
-                        failure {
-                            echo 'This will run only if failed'
-                        }
-                        unstable {
-                            echo 'This will run only if the run was marked as unstable'
-                        }
-                        changed {
-                            echo 'This will run only if the state of the Pipeline has changed'
-                            echo 'For example, if the Pipeline was previously failing but is now successful'
-                        }
-                    }
+                    // post {
+                    //     always {
+                    //         echo 'This will always run'
+                    //         script {
+                    //             helper.checkWhoAmI(env: params.env, branch: params.branch)
+                    //         }
+                    //     }
+                    //     success {
+                    //         echo 'This will run only if successful'
+                    //     }
+                    //     failure {
+                    //         echo 'This will run only if failed'
+                    //     }
+                    //     unstable {
+                    //         echo 'This will run only if the run was marked as unstable'
+                    //     }
+                    //     changed {
+                    //         echo 'This will run only if the state of the Pipeline has changed'
+                    //         echo 'For example, if the Pipeline was previously failing but is now successful'
+                    //     }
+                    // }
                 }
-                stage('Test') {
-                    when {
-                        expression {
-                            BRANCH_NAME == 'master'
-                        }
-                    }
-                    steps {
-                        echo 'Testing..'
-                    }
-                }
-                stage('Deploy') {
-                    when { branch 'master' }
-                    steps {
-                        echo 'Deploying....'
-                    }
-                }
+                // stage('Test') {
+                //     when {
+                //         expression {
+                //             BRANCH_NAME == 'master'
+                //         }
+                //     }
+                //     steps {
+                //         echo 'Testing..'
+                //     }
+                // }
+                // stage('Deploy') {
+                //     when { branch 'master' }
+                //     steps {
+                //         echo 'Deploying....'
+                //     }
+                // }
             }
         }
     }
